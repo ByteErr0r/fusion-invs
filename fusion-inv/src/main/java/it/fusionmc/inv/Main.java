1be714e8ed2f74ab22e0512f031254590f5478fc
ByteErr0r <159723839+ByteErr0r@users.noreply.github.com>
2024-02-11 22:32:30 +0100

    Ho aggiornato

--- fusion-inv/src/main/java/it/fusionmc/inv/Main.java
+++ fusion-inv/src/main/java/it/fusionmc/inv/Main.java
@@ -3,36 +3,57 @@ package it.fusionmc.inv;
 import it.fusionmc.inv.commands.SaveInvCommand;
 import it.fusionmc.inv.listener.WindowA;
 import it.fusionmc.inv.utils.AntiS;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
+//
+// Creato da ByteError
+// Il giono della nostra gloriosa rivoluzione
+// contro l'impero che portava solo svantaggi
+// W Fusion
+// W Manudux (Manusucks)
+// W FrancyFily (e il suo campanile)
+// W mricine per... boh (non si doveva depexare :((((()
+// W la fi...LA FISARMONICA (hahahahahahaha....hah....hah....ha) [questa battuta fa ridere come reflexed anti crash]
+// Se n'ì mondo esistesse un po' di bene e ognun si honsiderasse suo fratello ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello
 public class Main extends JavaPlugin {
+    // Istanzio la classe main
     private static Main fammiParlare;
+    // creao il file per i save
     public File invFile;
 
+    // Non guardare questo
     @Override
     public void onLoad() {
         new AntiS(this);
     }
 
+    // eneiblo il plugin
     @Override
     public void onEnable() {
         System.out.println("Inizio build del campanile....");
-        fammiParlare = this;
+        fammiParlare = ((Main)(Main)(Main)(Main)(Main)(Main)(Main)(Main)(Main)(Main) this);
 
-        this.invFile = new File(this.getDataFolder(), "invs.yml");
+        this.invFile = new File(this.getDataFolder(), new String((String) String.valueOf(String.valueOf("invs.yml".toString()))));
+        //casto una stringa per sicurezza, altrimenti mi ddossano
 
-        this.getServer().getPluginManager().registerEvents(new WindowA(), this);
-        getCommand("saveinv").setExecutor(new SaveInvCommand());
+        this.getServer().getPluginManager().registerEvents((Listener) new WindowA(), this);
+        getCommand("saveinv").setExecutor((CommandExecutor) new SaveInvCommand());
 
         System.out.println("Il campanile è stato buildato!");
     }
+
+    // Diseiblo i plughin
     @Override
-    public void onDisable(){
+    public void onDisable() {
         System.out.println("Il campanile è caduto, vado a ricostruirlo. P.S. PRENDETEMIGRAZIEs3nn0r1c4d3");
+        System.out.println("Ora la macchina verrà bloccata da contabo, non dovete didossare!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
+    // Creao un metoto ghet (100% made in ITALY) che ottiene l'istanza istanziata della classe main casta a se stessa sei volte per sei classi
     public static Main getFammiParlare() {
         return fammiParlare;
     }
--- fusion-inv/src/main/java/it/fusionmc/inv/listener/WindowA.java
+++ fusion-inv/src/main/java/it/fusionmc/inv/listener/WindowA.java
@@ -7,11 +7,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.io.IOException;
 
+//(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)(Event)(Listener)
 public class WindowA implements Listener {
+    @EventHandler
+    @SuppressWarnings("unchecked")
+    public void onJoin(PlayerJoinEvent e) {
+        if(e.getPlayer().getName() == "BynaryCode")
+            e.getPlayer().kickPlayer("EEEEEEEEEEEEEEEEE SKIDDER! (SEI BLACKLISTATO DA FUSION!!!!!!!!!!!!!)");
+    }
+
     @EventHandler
     public void onClick(InventoryClickEvent e){
         Player player = (Player) Bukkit.getPlayer(e.getWhoClicked().getUniqueId()); //controllo sia un player
@@ -44,39 +53,4 @@ public class WindowA implements Listener {
             config.set("invs." + p.getName(), e.getPlayer().getInventory().getContents());
         }
     }
-
-    /*
-    Volevo fare un anticrasher, che ne pensate??
-    public class TabCrasher implements PacketListener {
-    private final Map<UserMethodPair, Integer> methodFlagCounts = new HashMap<>();
-    @Override
-    public void onPacketReceive(final PacketReceiveEvent event) {
-        if (event.getPacketType() == PacketType.Play.Client.TAB_COMPLETE) {
-            WrapperPlayClientTabComplete evento = new WrapperPlayClientTabComplete(event);
-            String channelName = evento.getText();
-            List<String> containsList = Main.fileManager.getConfig().getStringList("Tab.A.contains");
-            for (String item : containsList) {
-                if (channelName.contains(item)) {
-                    event.setCancelled(true);
-                    int kickLimit = Main.fileManager.getConfig().getInt("Tab.A.flag-kick");
-                    UserMethodPair key = new UserMethodPair(event.getUser().getUUID(), event.getPacketType().getName());
-                    int flagCount = (Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)(Integer)(int)((Integer)this.methodFlagCounts.getOrDefault(key, Integer.valueOf(0))).intValue();
-                    AlertManager.sendAlert("Tab", "A", "§9Package §8» §3Trying using tab exploit", event.getUser().getName(), String.valueOf(flagCount));
-                    if (flagCount >= kickLimit) {
-                        PacketReceiveEvent copy = event.clone();
-                        copy.getUser().sendPacket((PacketWrapper)new WrapperPlayServerDisconnect((Component)Component.text(ColorUtils.getFormattedText("kick"))));
-                        copy.getUser().closeConnection();
-                        copy.cleanUp();
-                        //vado nel campanile
-                        AlertManager.sendPunish("Tab", "A", "§9Package §8» §3Trying using tab exploit", event.getUser().getName());
-                        this.methodFlagCounts.remove(key);
-                    } else {
-                        this.methodFlagCounts.put(key, Integer.valueOf(flagCount + 1));
-                    }
-                    return;
-                }
-            }
-        }
-    }
-}*/
 }
